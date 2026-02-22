d15a98a1a3abada8176f8c98af204da201581bef
Treemek <67553502+Treemek0@users.noreply.github.com>
2026-02-22 18:11:15 +0100

    Add files via upload

--- commands/level.js
+++ commands/level.js
@@ -20,11 +20,16 @@ module.exports = {
             const level = exp_module.xpToLevel ? exp_module.xpToLevel(xp) : exp_module.get_level(user, interaction.guild); // backwards compatibility
             const xpToNextLevel = exp_module.getTotalXPForLevel(level + 1) - xp;
 
+            const full = '█';
+            const empty = '░';
+            const progress = Math.round((xp / xpToNextLevel) * 20);
+            const progressBar = "|" + full.repeat(progress) + empty.repeat(20 - progress) + "|";
+
             const warningEmbed = new EmbedBuilder()
                     .setColor('Yellow')
                     .setTitle(`${user.username}`)
-                    .setDescription(`-# Ogólne XP: **${Math.round(xp)}**\n\n-# Poziom: **${level}**\n-# XP do następnego poziomu: **${Math.round(xpToNextLevel)}**`)
-                    .setFooter({ text: 'XP zdobywa się poprzez aktywność na serwerze!'})
+                    .setDescription(`-# Poziom: **${level}**\n-# Ogólne XP: **${Math.round(xp)}**\n\n${progressBar}`)
+                    .setFooter({ text: `XP do następnego poziomu: ${Math.round(xpToNextLevel)}`, iconURL: "https://raw.githubusercontent.com/Treemek0/Malach/main/imgs/XP_orb.gif"})
                     .setThumbnail(user.displayAvatarURL());
 
             interaction.reply({ embeds: [warningEmbed] });
